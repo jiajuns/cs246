71a6a542f9ec00c01856c03a2c8b61ccd1bc631c
Jiajun Sun <jiajunstanley@gmail.com>
2017-02-07 23:24:54 -0800

    pdf ready for submit

--- ProblemSet2/PCA.py
+++ ProblemSet2/PCA.py
@@ -1,6 +1,6 @@
 #%%
 import numpy as np
-from scipy import linalg
+from numpy import linalg
 
 ## initialize M
 #%%
@@ -9,7 +9,7 @@ M = np.array([[1, 2, 3, 4], [2, 1, 4, 3]])
 
 ## SVD of M
 #%%
-U, s, Vh = linalg.svd(np.transpose(M))
+U, s, Vh = linalg.svd(np.transpose(M), full_matrices=False)
 print('U: ', U)
 print('s: ', s)
 print('Vh ', Vh)
@@ -17,6 +17,6 @@ print('Vh ', Vh)
 
 ## eigenvalue decomposition of M^TM
 #%%
-Evals, Evcts = linalg.eigh(np.dot(M, np.transpose(M)))
+Evals, Evcts = linalg.eig(np.dot(M, np.transpose(M)))
 print('eigenvalues: ', Evals)
 print('eigenvectors: ', Evcts)
